b99771387721a2a1f58f828ecea7e074ae83faef
Robert Guggenberger <robertsadresse@gmx.de>
2018-09-19 12:45:03 +0200

    Make it installable with pip install -e .

--- setup.py
+++ setup.py
@@ -1,31 +1,27 @@
 from distutils.core import setup
 
-
 setup(
     name='pyArtacs',
     version='0.0.1',
-    description='Toolbox for removal of transcranial periodic current stimulation artifacts.',
-    long_description='A Python toolbox for periodic tCS artifact removal.',
+    description='Toolbox for removal of periodic artifacts.',
+    long_description='A Python toolbox for removal of artifact caused by transcranial periodic, especially alternating current stimulation (tACS).',
     author='Robert Guggenberger',
     author_email='robert.guggenberger@uni-tuebingen.de',
-    url='https://github.com/agricolab/pyArtacs'
+    url='https://github.com/agricolab/pyArtacs',
     download_url='https://github.com/agricolab/pyArtacs.git',
     license='MIT',
     packages=['artacs'],
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 2 - Pre-Alpha',
         'Environment :: Console',
         'Intended Audience :: Developers',
-        'Intended Audience :: Education',
         'Intended Audience :: Healthcare Industry',
         'Intended Audience :: Science/Research',
         'Intended Audience :: Information Technology',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6',
-        'Topic :: Scientific/Engineering :: Human Machine Interfaces',
-        'Topic :: Scientific/Engineering :: Medical Science Apps.',
-        'Topic :: Software Development :: Libraries',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Brain Stimulation',             
+        'Topic :: Scientific/Engineering :: Signal Processing',
         ]
 )
