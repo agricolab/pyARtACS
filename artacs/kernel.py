b58425d4bdab40a5f9598e624911b710b5d8e297
Robert Guggenberger <robertsadresse@gmx.de>
2018-09-20 14:56:50 +0200

    Implement multi-channel filtering
    Remove padding

--- artacs/kernel.py
+++ artacs/kernel.py
@@ -6,10 +6,13 @@ Created on Wed Sep 19 13:29:48 2018
 @author: rgugg
 """
 import numpy as np
+from typing import Tuple
+from numpy import ndarray
+from warnings import warn
 from scipy import signal
 from artacs.tools import resample_by_fs, resample_by_count
 # %%
-def _estimate_prms_from_kernel(kernel):
+def _estimate_prms_from_kernel(kernel:ndarray) -> Tuple[int, int]:
     '''estimate period and width from kernel
     
     args
@@ -23,8 +26,6 @@ def _estimate_prms_from_kernel(kernel):
         distance between two weight values in samples
     width:int
         number of weight values in each direction
-    direction:str {'left', 'right', 'sym'}
-        direction of kernel
 
     '''
   
@@ -32,65 +33,64 @@ def _estimate_prms_from_kernel(kernel):
     period = np.unique(np.diff(np.where(kernel!=0)[0]))
     if len(period) != 1:
         raise ValueError('Multiple or no periods recognized in kernel' + 
-                         'Was this kernel correctly constructed?')
+                         'Was the kernel correctly constructed?')
     
     period = int(period)
     left_width = (kernel[:kernel.shape[0]//2]<0).sum()
     right_width = (kernel[kernel.shape[0]//2:]<0).sum()        
     
     if left_width == 0:
-        direction = 'right'
         width = right_width
     elif right_width == 0:
-        direction = 'left'
         width = left_width
     elif right_width == left_width:
-        direction = 'sym'
         width = left_width # both need to be equal, so it doesn't matter
     else:
         raise ValueError('Kernel presents with unclear direction.' + 
-                         'Was this kernel correctly constructed?')
+                         'Was the kernel correctly constructed?')
  
-    return period, width, direction
+    return period, width
 
 # %%
-def _weigh_exp(width):
+def _weigh_exp(width:int) -> ndarray:
     'create exponential weights'    
     weights = signal.exponential(((width-1)*2)+1)[:width]
     weights /= (weights.sum())    
     return weights
 
-def _weigh_linear(width):
+def _weigh_linear(width:int)  -> ndarray:
     'create linear weights'    
-    weights = np.linspace(0, 1, num=width)
+    weights = np.linspace(1/width, 1, num=width)
     weights /= (weights.sum())    
     return weights
 
-def _weigh_gaussian(width, sigma = 1):
+def _weigh_gaussian(width:int, sigma:float=1) -> ndarray:
     'create gaussian weights'    
     weights = signal.gaussian(width*2, sigma)[0:width]
     weights /= ( weights.sum())
     return weights
 
-def _weigh_uniform(width):
+def _weigh_uniform(width:int) -> ndarray:
     'create uniform weights'    
     weights = np.ones(width)
     weights /= ( weights.sum())
     return weights
 
-def _weigh_not(width):
+def _weigh_not(width:int) -> ndarray:
     'create zero weights'
     weights = np.zeros(width)
     return weights
 
 def create_kernel(freq:int, fs:int, width:int, 
-                  left_mode:str='uniform', right_mode:str='uniform'):
+                  left_mode:str='uniform', 
+                  right_mode:str='uniform')  -> ndarray:
     
     in_period = fs/freq    
     period = int(np.ceil(in_period))   
     if in_period != period:
-        raise ValueError('Only integer periods are allowed.' + 
-                         'Try resampling your signal to higher sampling rate')
+        warn ('Only integer periods are natively supported.' + 
+              'Try resampling to higher sampling rate')
+        fs = int(period * freq)             
     
     weighfoos = {'uniform':_weigh_uniform,
                  'uni':_weigh_uniform,
@@ -108,57 +108,55 @@ def create_kernel(freq:int, fs:int, width:int,
     norm = left_weights.sum() + right_weights.sum()
     
     weights = np.hstack((-left_weights/norm, 1.0, -right_weights/norm))
+    
     midpoint = period*width    
     kernel = np.zeros((midpoint*2)+1)
     kernel[::period] = weights   
     return kernel
 
 # %%
-def kernel_filter(indata, freq:int, fs:int, width:int):        
+def _filter_1d(indata, fs:int, freq:int, kernel:ndarray):
     
-    in_channels = indata.shape[0]
-    in_samples = indata.shape[1]
+    in_samples = indata.shape[0]
     in_period = fs/freq
-    resample_flag = in_period != int(fs/freq)
-    
+        
+    #if sampling rate of signal and artifact are not integer divisible,
+    # we have to resample the data
+    resample_flag = ( in_period != int(np.ceil(in_period)) )     
     if resample_flag:
         old_fs = fs
-        period =int(np.ceil(in_period))
-        fs = int(period * freq)
-        #data = signal.resample(indata, int(in_samples/old_fs)*fs, axis=1)
-        outdata = []
-        for chan_idx, chan in enumerate(indata):
-            outdata.append(resample_by_fs(chan, up=fs, down = old_fs))        
-        data = np.asanyarray(outdata)
+        period = int(np.ceil(in_period))
+        fs = int(period * freq)        
+        data = resample_by_fs(indata, up=fs, down=old_fs)        
     else:
         data = indata
         period = int(in_period)        
+
+    # if the kernel period is not matching the artifact period, 
+    # filtering would be off
+    kperiod, kwidth =  _estimate_prms_from_kernel(kernel)
+    if kperiod != period:
+        raise ValueError('Kernel is not matching artifact frequency. ' +
+                         'Was the kernel correctly constructed?')
+
     #-------------------------------------------------------------------------
-    
-    k = np.zeros((period*width*2)+1)
-    k[0::period] = -1/(width*2)
-    k[width*period] = 1
-    pad_width = (period*width)
-    
-    fdata = np.zeros((in_channels, data.shape[1]))    
-    for idx, c in enumerate(data):
-        # pad 
-        padded_channel = np.pad(c, pad_width, 'wrap')
-        filt_channel = np.convolve(padded_channel, k, 'same')
-        # remove padding
-        fdata[idx,:] = filt_channel[pad_width:-pad_width]
-        
-    
+    fdata = np.convolve(data, kernel[::-1], 'same')
     #-------------------------------------------------------------------------
     if resample_flag:
-        filtered = []
-        for chan_idx, chan in enumerate(fdata):
-            filtered.append(resample_by_count(chan, in_samples))        
-        filtered = np.asanyarray(filtered)
-        
+        filtered = resample_by_count(fdata, in_samples)
+        filtered = np.asanyarray(filtered)    
     else:
         filtered = fdata            
         
+    return filtered
+    
+# %%
+def _filter_2d(indata:ndarray, freq:int, fs:int, kernel:ndarray):        
+    
+    filtered = np.zeros(indata.shape)
+    for idx, chandata in enumerate(indata):        
+        filtered[idx,:] = _filter_1d(chandata, freq, fs, kernel)
+    
     return filtered
 #%%
 class KernelFilter():
--- test/test.py
+++ test/test.py
@@ -4,7 +4,7 @@
 
 
 '''
-from artacs.kernel import create_kernel
+from artacs.kernel import create_kernel, _estimate_prms_from_kernel, _filter_1d, _filter_2d
 import numpy as np
 #%%
 def test_kernel():
@@ -13,21 +13,24 @@ def test_kernel():
     width = 5
     period = int(np.ceil(fs/freq))
     kernel = create_kernel(freq, fs, width)
-    assert kernel.sum() == 0
+    assert np.isclose(kernel.sum(), 0.0)
     assert kernel.shape[0] == 1001
+    assert(_estimate_prms_from_kernel(kernel) == (period, width))
     
     kernel = create_kernel(freq, fs, width, 
                            left_mode='uniform',right_mode ='None')
-    assert kernel.sum() == 0
+    assert np.isclose(kernel.sum(), 0.0)
     assert kernel.shape[0] == 1001
+    assert(_estimate_prms_from_kernel(kernel) == (period, width))
     assert np.all(np.isclose(kernel[::period],
                              np.array([-0.2, -0.2, -0.2, -0.2, -0.2,  1., 
                                        -0. , -0. , -0. , -0. , -0. ])))
     
     kernel = create_kernel(freq, fs, width, 
                            left_mode='gauss',right_mode ='exp')
-    assert kernel.sum() == 0
+    assert np.isclose(kernel.sum(), 0.0)
     assert kernel.shape[0] == 1001
+    assert(_estimate_prms_from_kernel(kernel) == (period, width))
     assert np.all(np.isclose(kernel[::period],
                              np.array([-1.59837446e-05, -8.72682888e-04,
                                        -1.75283044e-02, -1.29517624e-01,
@@ -38,12 +41,71 @@ def test_kernel():
     
     kernel = create_kernel(freq, fs, width, 
                            left_mode='linear',right_mode ='uniform')
-    assert kernel.sum() == 0
+    assert np.isclose(kernel.sum(), 0.0)
     assert kernel.shape[0] == 1001
+    assert(_estimate_prms_from_kernel(kernel) == (period, width))
     assert np.all(np.isclose(kernel[::period],
-                             np.array([-0.  , -0.05, -0.1 , -0.15, -0.2 ,  1., 
-                                       -0.1 , -0.1 , -0.1 , -0.1 , -0.1 ])))
+                             np.array([-0.03333333, -0.06666667, -0.1,
+                                       -0.13333333, -0.16666667, 1. , -0.1,
+                                       -0.1, -0.1, -0.1,-0.1])))
     
+    
+    # test directionality of kernel
+    fs = 1000
+    freq = 10
+    period = int(np.ceil(fs/freq))
+    duration_in_s = 2
+    t = np.linspace(1/fs, duration_in_s, num=fs*duration_in_s)
+    data = np.zeros(np.shape(t))
+    data[data.shape[0]//2] = 1
+    kernel = create_kernel(freq, fs, 1, 
+                           left_mode='uniform', right_mode ='none')
+    filtered = _filter_1d(data, fs, freq, kernel)
+    assert np.where(filtered==1.0)[0][0] == 1000
+    assert np.where(filtered==-1.0)[0][0] == 1100
+    
+    
+    # test kernel removes sine perfectly
+    fs = 1000
+    freq = 10
+    period = int(np.ceil(fs/freq))
+    duration_in_s = 2
+    t = np.linspace(1/fs, duration_in_s, num=fs*duration_in_s)
+    data = np.sin(2*np.pi*freq*t)    
+    kernel = create_kernel(freq, fs, 1, 
+                           left_mode='uniform', right_mode ='none')
+    filtered = _filter_1d(data, fs, freq, kernel)
+    assert np.all(np.isclose(filtered[period:], 0, 1e-10))
+
+    # test kernel removes sine somewhat well when fs mismatch
+    fs = 1000
+    freq = 15
+    for freq in range(10, 21, 1):        
+        period = int(np.ceil(fs/freq))
+        kernel = create_kernel(freq, fs, 1, 
+                               left_mode='uniform', right_mode ='none')
+        t = np.linspace(1/fs, duration_in_s, num=fs*duration_in_s)
+        data = np.sin(2*np.pi*freq*t)    
+        filtered = _filter_1d(data, fs, freq, kernel)
+        print('{:3.0f} '.format(freq),end='')
+        print(np.max(np.abs(filtered[period*2:])))
+        assert np.all(np.isclose(filtered[period*2:], 0, atol=1e-04))
+
+    # test multi-channel filter
+    fs = 1000
+    freq = 10
+    period = int(np.ceil(fs/freq))
+    duration_in_s = 2
+    t = np.linspace(1/fs, duration_in_s, num=fs*duration_in_s)
+    data = np.sin(2*np.pi*freq*t)    
+    data = np.vstack((data, data))
+    kernel = create_kernel(freq, fs, 1, 
+                               left_mode='uniform', right_mode ='none')
+    filtered = _filter_2d(data, fs, freq, kernel)
+    for chan in filtered:
+        assert np.all(np.isclose(chan[period:], 0, 1e-10))
+
+
     print('Test successful')
     
 # %%
